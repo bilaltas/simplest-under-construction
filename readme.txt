221b133e71e6223e40baadee220522c73303a3fd
Bilal TAS <bilaltas@me.com>
2017-04-27 15:38:08 +0300

    Initial release

---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Simplest Under Construction ===
+Contributors: bilaltas
+Tags: coming soon, under construction, maintenance, maintenance mode
+Requires at least: 3.0
+Tested up to: 4.7.4
+
+This is the simplest plugin to restrict a Wordpress site to the public. You can allow them by IP or user role.
+
+== Description ==
+This is the simplest plugin to restrict a Wordpress site to the public. You can allow them by IP or user role.
+
+== Installation ==
+=For Installation=
+1. Login to your website and go to the Plugins section of your admin panel.
+2. Click the Add New button.
+3. Under Install Plugins, click the Upload link.
+4. Select the plugin zip file (simplest-under-construction.zip) from your computer then click the Install Now button.
+5. You should see a message stating that the plugin was installed successfully.
+6. Click the Activate Plugin link.
+
+== Changelog ==
+= 0.1 (2017-04-27 15:33 EET) =
+* Initial release
---
+++ simplest-under-construction.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Simplest Under Construction
+Description: This is the simplest plugin to restrict a Wordpress site to the public. You can allow them by IP or user role.
+Author: Bilal TAS
+Author URI: http://bilaltas.net
+Version: 0.1
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+define( 'SUC_FILE', __FILE__ );
+define( 'SUC_DEBUG', false );
+
+
+// Front-End
+if ( !is_admin() )
+	require_once( dirname( SUC_FILE ).'/suc_files/under_construction_public.php' );
+
+
+// Back-End
+else
+	require_once( dirname( SUC_FILE ).'/suc_files/under_construction.php' );
+
+
+// For Both
+require_once( dirname( SUC_FILE ).'/suc_files/under_construction_admin-public_functions.php' );
+
+
+// Check the plugin updates
+require 'plugin-update-checker/plugin-update-checker.php';
+$sucUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/bilaltas/simplest-under-construction/',
+	__FILE__,
+	'simplest-under-construction'
+);
